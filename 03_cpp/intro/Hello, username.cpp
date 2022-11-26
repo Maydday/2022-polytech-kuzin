f512977d01226f22cb7a7ef4bea523fd5c794dcb
Mayder <kuzirmixail@gmail.com>
2022-11-26 18:50:19 +0300

    feat: Hello, username.cpp

---
+++ 03_cpp/intro/Hello, username.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int name;
+    cout << "Enter your name: ";
+    cin >> name; // Считываем полученное знаечение
+    cout << "Hello, " << name;
+    return 0;
+}
