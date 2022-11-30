ab75447f229dd538bc9daad4f6dd71138b52cd4c
Mayder <kuzirmixail@gmail.com>
2022-11-30 22:17:08 +0300

    feat: palindrome_check

--- 06_pointers_references/palindrome/palindrome.cpp
+++ 06_pointers_references/palindrome/palindrome.cpp
@@ -1,3 +1,24 @@
-//
-// Created by kuzir on 30.11.2022.
-//
+#include<cassert>
+#include<cstring>
+
+bool palindrome_check(const char* s)
+{
+    int k = 0;
+    int n = strlen(s) - 1;
+    for(int i = 0, k = n; i <= k; i++, k--)
+    {
+        if(s[i] != s[k])
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    assert(palindrome_check("b") == true);
+    assert(palindrome_check("taco cat") == true);
+    assert(palindrome_check("well") == false);
+    assert(palindrome_check(nullptr) == true);
+    assert(palindrome_check(" ") == true);
+    return  0;
+}
