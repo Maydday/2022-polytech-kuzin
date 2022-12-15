ec2a2454b028f9adb0213196951dc2c9a0db0eb6
Mayder <kuzirmixail@gmail.com>
2022-12-16 00:51:24 +0300

    add: test_queue.cpp

--- 10_list/queue/test_queue.cpp
+++ 10_list/queue/test_queue.cpp
@@ -1,3 +1,19 @@
-//
-// Created by kuzir on 16.12.2022.
-//
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "queue.hpp"
+#include <doctest.h>
+
+TEST_CASE("what?") {
+
+    Queue q;
+
+    q.enqueue(1);
+    q.enqueue(2);
+    q.dequeue();
+    q.dequeue();
+    q.enqueue(3);
+    q.enqueue(4);
+    q.enqueue(5);
+    q.dequeue();
+    CHECK((q.front)->data == 4);
+    CHECK((q.rear)->data == 5);
+}
