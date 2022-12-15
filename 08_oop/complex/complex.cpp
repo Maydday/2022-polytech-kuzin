92b11cc4356ed674680bce828af4feb5db37c64a
Mayder <kuzirmixail@gmail.com>
2022-12-15 20:01:47 +0300

    fixed

--- 08_oop/complex/complex.cpp
+++ 08_oop/complex/complex.cpp
@@ -2,20 +2,17 @@
 #include "complex.h"
 #include <doctest.h>
 
+const Complex Complex::operator+(const Complex rv) const {
+    return Complex(r_part + rv.r_part, i_part + rv.i_part);
+}
+const Complex Complex::operator-(const Complex rv) const {
+    return Complex(r_part - rv.r_part, i_part - rv.i_part);
+}
 
-const Complex Complex::operator+(const Complex rv) const
-{
-    return Complex(x + rv.x, y + rv.y);
+const bool Complex::operator==(const Complex rv) const {
+    return r_part == rv.r_part && i_part == rv.i_part;
 }
-const Complex Complex::operator-(const Complex rv) const
-{
-    return Complex(x - rv.x, y - rv.y);
-}
-const bool Complex::operator==(const Complex rv) const
-{
-    return x == rv.x && y == rv.y;
-}
-const double Complex::abs() const
-{
-    return sqrt(x * x + y * y);
+
+const double Complex::abs() const {
+    return sqrt(r_part * r_part + i_part * i_part);
 }
--- 08_oop/complex/complex.h
+++ 08_oop/complex/complex.h
@@ -1,9 +1,8 @@
-class Complex
-{
+class Complex {
 public:
-    int x;
-    int y;
-    Complex(int r, int i) : x(r), y(i) {};
+    int r_part;
+    int i_part;
+    Complex(int r, int i) : r_part(r), i_part(i) {};
     const Complex operator+(const Complex rv) const;
     const Complex operator-(const Complex rv) const;
     const bool operator==(const Complex rv) const;
